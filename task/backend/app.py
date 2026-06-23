94b3f56c8395d66e128808f4cb32c62c7f27745d
kamelia ziyan <kameliaziyan11gmail.com@kamelias-MacBook-Air.local>
2026-06-23 19:49:44 +0300

    Update CORS to allow Vercel domain

--- task/backend/app.py
+++ task/backend/app.py
@@ -30,7 +30,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://lotus-task.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
